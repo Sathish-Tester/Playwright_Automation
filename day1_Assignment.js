8eb84cbb55a144e2ad5666e9f6f06e913c6182a4
Sathish-Tester <sathishajs7@gmail.com>
2026-03-01 14:11:42 +0530

    Refactor console.log statements to template literals

--- day1_Assignment.js
+++ day1_Assignment.js
@@ -5,11 +5,11 @@ let knowsAutomation = true;
 let hasPlaywright;
 
 // Print the values of the variables
-console.log("User Name: " + userName);
-console.log("Organization: " + organization);
-console.log("Contact Number: " + contactNo);
-console.log("Knows Automation: " + knowsAutomation);
-console.log("Has Playwright: " + hasPlaywright);
+console.log(`User Name: ${userName}`);
+console.log(`Organization: ${organization}`);
+console.log(`Contact Number: ${contactNo}`);
+console.log(`Knows Automation: ${knowsAutomation}`);
+console.log(`Has Playwright: ${hasPlaywright}`);
 
 // Print the data types of the variables
 console.log("Data type is " + typeof userName);
@@ -17,4 +17,5 @@ console.log("Data type is " + typeof organization);
 console.log("Data type is " + typeof contactNo);
 console.log("Data type is " + typeof knowsAutomation);
 console.log("Data type is " + typeof hasPlaywright);
+
 
