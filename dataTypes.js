dd20b3f4125db6f519580691f2c435bd2e46bfd1
Sathish-Tester <sathishajs7@gmail.com>
2026-03-01 14:09:41 +0530

    Add files via upload

---
+++ dataTypes.js
@@ -0,0 +1,21 @@
+var firstName = "John";
+var companyName = 'Microsoft';
+var mobileNumber = 1234567890;
+var isAutomation = true;
+var hasPlaywright;
+
+
+// Print the values of the variables
+console.log("First Name: " + firstName);
+console.log("Company Name: " + companyName);
+console.log("Mobile Number: " + mobileNumber);
+console.log("Is Automation: " + isAutomation);
+console.log("Has Playwright: " + hasPlaywright);
+
+// Print the data types of the variables
+console.log("Data type is " + typeof firstName);
+console.log("Data type is " + typeof companyName);
+console.log("Data type is " + typeof mobileNumber);
+console.log("Data type is " + typeof isAutomation);
+console.log("Data type is " + typeof hasPlaywright);
+
---
+++ day1_Assignment.js
@@ -0,0 +1,20 @@
+let userName = "Sathish";
+let organization = 'Comcast';
+let contactNo = 1234567890;
+let knowsAutomation = true;
+let hasPlaywright;
+
+// Print the values of the variables
+console.log("User Name: " + userName);
+console.log("Organization: " + organization);
+console.log("Contact Number: " + contactNo);
+console.log("Knows Automation: " + knowsAutomation);
+console.log("Has Playwright: " + hasPlaywright);
+
+// Print the data types of the variables
+console.log("Data type is " + typeof userName);
+console.log("Data type is " + typeof organization);
+console.log("Data type is " + typeof contactNo);
+console.log("Data type is " + typeof knowsAutomation);
+console.log("Data type is " + typeof hasPlaywright);
+
